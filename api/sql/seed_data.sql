a054fd853da0476b5da2d182e51d0936e9d4c4f7
victor <ledzepp1600@gmail.com>
2022-02-05 14:43:14 -0600

    Fixed courses images

--- api/sql/seed_data.sql
+++ api/sql/seed_data.sql
@@ -1,3 +1,10 @@
+INSERT INTO api_course (id, name, description, created_at, icon, active) VALUES (1, 'Matemáticas', '', '2022-02-05 20:39:00.531232', 'photos/icons/2022/02/05/math.jpg', 1);
+INSERT INTO api_course (id, name, description, created_at, icon, active) VALUES (2, 'Lenguaje', '', '2022-02-05 20:39:25.054127', 'photos/icons/2022/02/05/lenguaje.jpg', 1);
+INSERT INTO api_course (id, name, description, created_at, icon, active) VALUES (3, 'Biologia', '', '2022-02-05 20:39:39.337348', 'photos/icons/2022/02/05/biologia.png', 1);
+INSERT INTO api_course (id, name, description, created_at, icon, active) VALUES (4, 'Sociales', '', '2022-02-05 20:39:54.577533', 'photos/icons/2022/02/05/sociales.jpg', 1);
+INSERT INTO api_course (id, name, description, created_at, icon, active) VALUES (5, 'Química', '', '2022-02-05 20:40:23.655649', 'photos/icons/2022/02/05/qumica.png', 1);
+INSERT INTO api_course (id, name, description, created_at, icon, active) VALUES (6, 'Física', '', '2022-02-05 20:40:57.274821', 'photos/icons/2022/02/05/fisica.png', 1);
+
 -- user
 INSERT INTO USERS(id, password, last_login, is_superuser, first_name, last_name, is_staff, is_active, date_joined, email) VALUES (1, '!H1HpGky6GD5rhKcjWW3SqwKPhAdsBrPsMHr2d2cx', null, true, '', '', true, true, '2021-10-06 18:41:11.160023', 'diana@gmail.com');
 
