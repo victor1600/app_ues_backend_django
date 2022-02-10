c38b286f858041cd23d5213f0a5c04d08dbac0ce
victor <ledzepp1600@gmail.com>
2022-02-10 11:53:00 -0600

    Created custom signal for cleaning up orphan images/files uploaded by tests.

--- api/signals/handlers.py
+++ api/signals/handlers.py
@@ -1,16 +1,21 @@
 from django.dispatch import receiver
+from django.conf import settings
 from api.signals import test_finished
 import logging
+import os
 
 logger = logging.getLogger()
 
 
 @receiver(test_finished)
 def on_test_finished(sender, **kwargs):
+    """
+    This handler will cleanup the media files created by the
+    test.
+    """
     logger.info("cleaning up media files created by pytest.")
-    media_url = kwargs['media_url']
-    # TODO: to this media url, append base dir:
-    #  media/photos/icons/2022/02/08/biologia_1fBIIV4.png
+    media_partial_url = kwargs['media_url']
+    media_file = os.path.join(settings.BASE_DIR, media_partial_url)
+    os.remove(media_file)
+    logger.info(f'Deleted {media_file}')
 
-    # TODO: Delete media path.
-    logger.info(media_url)
