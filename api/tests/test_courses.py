6112234db66fb5dfcc17d1f135c667dec169b4ba
victor <ledzepp1600@gmail.com>
2022-02-18 09:31:39 -0600

    Initial tests for courses

--- api/tests/test_courses.py
+++ api/tests/test_courses.py
@@ -3,6 +3,10 @@ from rest_framework import status
 from api.signals import test_finished
 from api.models import Curso
 from model_bakery import baker
+from django.conf import settings
+import requests
+from PIL import Image
+from io import StringIO
 
 import pytest
 
@@ -37,11 +41,13 @@ logger = logging.getLogger()
 
 @pytest.mark.django_db
 class TestRetrieveCourse:
+    @pytest.mark.skip
     def test_if_user_is_anonymous_returns_401(self, api_client):
         course = baker.make(Curso)
         response = api_client.get(f'/api/courses/{course.id}/')
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
+    @pytest.mark.skip
     def test_if_course_exists_returns_200(self, api_client, authenticate):
         course = baker.make(Curso)
         authenticate()
@@ -50,4 +56,13 @@ class TestRetrieveCourse:
         assert response.status_code == status.HTTP_200_OK
 
         # TODO: create test case to test if img is visible!!
+    # def test_img_visible(self, api_client, authenticate):
+    #     image_url = f'{settings.BASE_DIR}/icons/biologia.png'
+    #     course = Curso.objects.create(texto='a', icono=image_url)
+    #     authenticate()
+    #     response = api_client.get(f'/api/courses/{course.id}/')
+    #     print(response.data['icono'])
+    #     r = requests.get(response.data['icono'])
+    #     print(r.content)
+    #     assert False
 
