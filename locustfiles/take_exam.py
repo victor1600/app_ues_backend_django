7ed456c5aa94c26a5656bfbe2732e5be1ba51a11
victor <ledzepp1600@gmail.com>
2022-02-19 16:00:15 -0600

    Created initial locust files for Load Test

---
+++ locustfiles/take_exam.py
@@ -0,0 +1,32 @@
+from locust import HttpUser, task, between
+from random import randint
+from logging import getLogger
+
+logger = getLogger()
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task(1)
+    def view_topics(self):
+        logger.info('[LOAD TEST] Viewing topics')
+        # TODO: change this according to actual data.
+        course_id = randint(1, 5)
+        self.client.get(f'/api/topics/?curso={course_id}', name='/api/topics')
+
+    @task(2)
+    def view_topic(self):
+        logger.info('[LOAD TEST] Viewing a topic')
+        topic_id = randint(1, 5)
+        self.client.get(f'/api/topics/{topic_id}',
+                        name='/api/topics/:id')
+
+    @task(4)
+    def get_exam(self):
+        logger.info('[LOAD TEST] Getting exam')
+        topic_id = randint(1, 5)
+        self.client.get(f'/api/exam-questions/?tema={topic_id}', name='/api/exam-questions/')
+
+
+
